95b091f426ce04ba0c6ec360537954ce21e594fe
itachi-p <itachip38@gmail.com>
2024-10-28 21:01:02 +0900

    確認作業完了

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -2,15 +2,15 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
+    // @ResponseBody
     public String home() {
-        System.out.println("HomeController is called");
+        // System.out.println("HomeController is called");
         return "index";
     }
 }
